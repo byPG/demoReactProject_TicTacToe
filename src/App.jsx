7f76d38ee4d1f03591883e63260d3d2679f30c66
byPG <67753466+byPG@users.noreply.github.com>
2025-05-06 20:53:22 +0200

    reducing state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 import { useState } from "react";
 
 
-import Player from "./assets/components/Player";
-import Gameboard from "./assets/components/GameBoard";
-import Log from "./assets/components/Log";
+import Player from "./assets/components/Player.jsx";
+import Gameboard from "./assets/components/GameBoard.jsx";
+import Log from "./assets/components/Log.jsx";
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+
+if (gameTurns.length > 0 && gameTurns[0].player === 'X'){
+  currentPlayer = 'O';
+}
+
+return currentPlayer;
+}
 
 function App() {
 
 const [gameTurns, setGameTurns] = useState([]);
-const [activePlayer, setActivePlayer] = useState('X');
+// const [activePlayer, setActivePlayer] = useState('X');
+
+const activePlayer = deriveActivePlayer(gameTurns);
+
 
 function handleSelectSquare(rowIndex, colIndex){
-  setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
   setGameTurns(prevTurns => {
-    let currentPlayer = 'X';
-
-    if (prevTurns.length > 0 && prevTurns[0].player === 'X'){
-      currentPlayer = 'O';
-    }
-
+    const currentPlayer = deriveActivePlayer(prevTurns);
 
     const updatedTurns = [{square: {row: rowIndex, col: colIndex}, player: activePlayer}, ...prevTurns];
 
