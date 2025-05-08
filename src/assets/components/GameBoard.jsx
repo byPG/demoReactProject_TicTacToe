eaa05750f59d9092bbf08f0306a4b446e4537018
byPG <67753466+byPG@users.noreply.github.com>
2025-05-08 16:18:33 +0200

    better appearance of code

--- src/assets/components/GameBoard.jsx
+++ src/assets/components/GameBoard.jsx
@@ -1,16 +1,22 @@
-
-export default function Gameboard({onSelectSquare, board}){
-
-    return(
-        <ol id="game-board">
-          {board.map((row, rowIndex )=> <li key={rowIndex}>
-
-            <ol>
-                {row.map((playerSymbol, coldIndex) => (<li key={coldIndex}>
-                    <button onClick={() => onSelectSquare(rowIndex, coldIndex)} disabled={playerSymbol !== null}> {playerSymbol}</button>
-                    </li>))}
-            </ol>
-          </li>)}
-        </ol>
-    )
+export default function Gameboard({ onSelectSquare, board }) {
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, coldIndex) => (
+              <li key={coldIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, coldIndex)}
+                  disabled={playerSymbol !== null}>
+                  {" "}
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
